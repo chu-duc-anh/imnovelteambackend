6e9f8e48d24f25d6783b55928863f4a883652997
chu-duc-anh <142377947+chu-duc-anh@users.noreply.github.com>
2025-08-09 15:24:40 +0700

    Update settingModel.js

--- models/settingModel.js
+++ models/settingModel.js
@@ -10,7 +10,6 @@ const settingSchema = mongoose.Schema({
         virtuals: true,
         transform(doc, ret) {
             ret.id = ret._id;
-            delete ret._id;
             delete ret.__v;
         }
     }
