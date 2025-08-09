c6c81aff1faf46ddfd2948ff631dd15b4fac9171
chu-duc-anh <142377947+chu-duc-anh@users.noreply.github.com>
2025-08-09 11:05:54 +0700

    Update SettingRoutes.js

--- routes/SettingRoutes.js
+++ routes/SettingRoutes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 import { getSettings, updateSettings } from '../controllers/settingController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
+// @desc    Routes for site settings
+// @route   /api/settings
+
 router.route('/')
-    .get(getSettings)
-    .put(protect, admin, updateSettings);
+    .get(getSettings) // Anyone can get settings
+    .put(protect, admin, updateSettings); // Only admins can update
 
 export default router;
